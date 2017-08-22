e93a21a7b0e9e34706527686bdcfc848afa5c211
santosh suryawanshi <santosh.suryawanshi28@gmail.com>
2017-08-22 19:11:12 +0530

    Displaying Repeating Information(ng-repeat)

--- client/js/controller.js
+++ client/js/controller.js
@@ -10,6 +10,36 @@ angular.module('eventApp')
             city:'Pune',
             state:'Maharashtra'
         },
-        imgUrl:'/images/angularjs-logo.png'
+        imgUrl:'/images/angularjs-logo.png',
+        sessions:[
+            {
+                name:'JavaScript ',
+                creatorName :'Alen',
+                duration:'1 hr',
+                level:'Introductory',
+                abstract:'This session will take closer look at JavaScript Object and inheritance'
+            },
+            {
+                name:'Node ',
+                creatorName :'Ryan Dahl',
+                duration:'2 hr',
+                level:'Advanced',
+                abstract:'Learn Node.js from scratch and become zero to hero'
+            },
+            {
+                name:'Angular ',
+                creatorName :'Misko Hevery',
+                duration:'30 min',
+                level:'Intermediate',
+                abstract:'Learn Angualr js introduction'
+            },
+            {
+                name:'Ember',
+                creatorName :'Yehuda Katz',
+                duration:'30 min',
+                level:'Advanced',
+                abstract:'Learn Ember js fully setp by step'
+            }
+        ]
     }
 }]);
